fbd2c41c7d166b6499064f17b88470dab943836d
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-01-27 16:33:36 +0100

    feat: Limited framerate to 60 FPS

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         # Exit on Quit
         for event in pygame.event.get():
@@ -13,6 +15,9 @@ def main():
         
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        # Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000 # ms
 
 
 if __name__ == "__main__":
