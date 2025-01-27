9c8f9a5bfcaf2bd06b98872af5e265cd27ce28dd
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-01-27 16:52:02 +0100

    feat: Added player class.

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # Exit on Quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill((0,0,0))
+
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         # Limit framerate to 60 FPS
-        dt = clock.tick(60) / 1000 # ms
+        dt = clock.tick(60) / 1000  # ms
 
 
 if __name__ == "__main__":
