9ab30a3beab1fc5eed32eb2399c8beb58158237f
Marek Jakóbiak <marek.jakobiak@gmail.com>
2025-01-27 17:18:08 +0100

    refac: Moved objects into containers.

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (drawables, updatables)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -16,10 +22,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for updatable in updatables:
+            updatable.update(dt)
 
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for drawable in drawables:
+            drawable.draw(screen)
+
         pygame.display.flip()
 
         # Limit framerate to 60 FPS
